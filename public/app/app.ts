0d992f428b66d1fcd8a85289e6d3cfaf705412e0
Joe Pea <joe@trusktr.io>
2025-08-04 02:12:08 -0700

    split drippy-scene to a separate file

--- public/app/app.ts
+++ public/app/app.ts
@@ -2,9 +2,9 @@ import {html, Element, element, css, signal, onCleanup} from 'lume'
 import '../routes.js' // track page visits
 import '../elements/login-ui.js'
 import '../elements/theme-switch.js'
+import './drippy-scene.js'
 
 const avatarThumb = new URL('../images/avatar-female-tmp.png', import.meta.url)
-const femaleAvatar = new URL('../models/EM-Female.glb', import.meta.url)
 
 // Hide the loading cover
 const loadingCover = document.getElementById('loadingCover')
@@ -163,81 +163,6 @@ export class DrippyApp extends Element {
 					height: auto;
 				}
 			}
-		}
-	`
-}
-
-@element
-export class DrippyScene extends Element {
-	static elementName = 'drippy-scene'
-
-	@signal isDark = false
-
-	connectedCallback() {
-		super.connectedCallback()
-
-		this.isDark = document.documentElement.dataset.theme === 'dark'
-
-		const onThemeChange = () => (this.isDark = document.documentElement.dataset.theme === 'dark')
-
-		this.createEffect(() => {
-			const mo = new MutationObserver(onThemeChange)
-			mo.observe(document.documentElement, {attributes: true, attributeFilter: ['data-theme']})
-			onCleanup(() => mo.disconnect())
-		})
-	}
-
-	template = () => html`
-		<lume-scene webgl>
-			<lume-point-light
-				position="500 -500 500"
-				intensity="2000"
-				prop:color=${() => (this.isDark ? 'cyan' : 'white')}
-			></lume-point-light>
-			<lume-point-light
-				position="-500 500 -500"
-				intensity="2000"
-				prop:color=${() => (this.isDark ? 'deeppink' : 'white')}
-			></lume-point-light>
-			<lume-point-light
-				position="500 -500 -500"
-				intensity="2000"
-				prop:color=${() => (this.isDark ? 'royalblue' : 'white')}
-			></lume-point-light>
-			<lume-point-light
-				position="-500 500 500"
-				intensity="2000"
-				prop:color=${() => (this.isDark ? 'orange' : 'white')}
-			></lume-point-light>
-
-			<lume-camera-rig
-				min-distance="1.5"
-				max-distance="8"
-				distance="4"
-				dolly-speed="0.01"
-				position="0 -1 0"
-			></lume-camera-rig>
-
-			<lume-box
-				visible="false"
-				cast-shadow="false"
-				size="1 1 1"
-				color="skyblue"
-				roughness="0.3"
-				metalness="0.7"
-				mount-point="0.5 0.5 0.5"
-			></lume-box>
-
-			<lume-gltf-model src=${femaleAvatar.href}></lume-gltf-model>
-		</lume-scene>
-	`
-
-	css = css/*css*/ `
-		:host {
-			width: 600px;
-			height: 400px;
-
-			touch-action: none;
 		}
 	`
 }
