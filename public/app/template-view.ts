e7cf15e7188c5f4568e4c764ac0d9c95ff99a0d2
thidieuanhle <anhlee2101@gmail.com>
2025-09-27 16:58:05 +0200

    fix: add flex-shrink and height auto to prevent container taking up all space and hide product text

--- public/app/template-view.ts
+++ public/app/template-view.ts
@@ -411,7 +411,7 @@ export class TemplateView extends Element {
 																oncardselected=${this.#onItemClick}
 																object-fit="contain"
 																object-position="center"
-																aspect-ratio=${() => (window.innerWidth <= 768 ? '1' : '0.79')}
+																aspect-ratio="0.79"
 															></item-card>
 															<show-when
 																condition=${() => this.showTemplateOverlay?._id === template._id}
@@ -545,14 +545,8 @@ export class TemplateView extends Element {
 			position: relative;
 			width: 100%;
 			aspect-ratio: 0.79;
-			isolation: isolate;
-			contain: layout;
-		}
-
-		@media (max-width: 768px) {
-			.template-item-container {
-				aspect-ratio: 1;
-			}
+			flex-shrink: 0;
+			height: auto;
 		}
 
 		.template-product-name {
