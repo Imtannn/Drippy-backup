938301f775d00677b0ffc7b1a94e2d7172060598
Joe Pea <joe@lume.io>
2025-09-29 00:22:28 -0700

    move app import to the dependent file, add pointer cursor to scene thumbnails

--- public/app/spaces-selection.ts
+++ public/app/spaces-selection.ts
@@ -371,6 +371,7 @@ export class SpacesSelection extends Element {
 			justify-content: center;
 			overflow: hidden;
 			border-radius: var(--borderRadiusLarge);
+			cursor: pointer;
 
 			img {
 				object-fit: cover;
