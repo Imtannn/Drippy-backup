52446d334c8183b8a59c9b48d1261a88ef153c4e
Joe Pea <joe@lume.io>
2025-10-05 10:02:01 -0700

    use nested effects to simplify and reduce code in lume-animation

--- public/elements/lume-animation.ts
+++ public/elements/lume-animation.ts
@@ -1,16 +1,19 @@
 import {
 	booleanAttribute,
+	createEffect,
+	createSignal,
 	css,
 	Element,
 	element,
 	Element3D,
-	signal,
+	GltfModel,
+	onCleanup,
 	stringAttribute,
-	untrack,
 	type ElementAttributes,
 } from 'lume'
 import * as THREE from 'three'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+import {onModelLoad} from '../utils.js'
 
 type LumeAnimationAttributes = 'src' | 'clipName' | 'additive' | 'paused'
 
@@ -52,158 +55,109 @@ export class LumeAnimation extends Element {
 
 	// TODO: Add attributes like `blending`, `time`, `mixer`, etc.
 
-	@signal isParentModelLoaded = false
-
-	@signal clips: THREE.AnimationClip[] = []
-
-	#mixer: THREE.AnimationMixer | null = null
-
-	#clock = new THREE.Clock()
-
-	// TODO: Make this a map of clip actions so they can be cached.
-	#clipAction: THREE.AnimationAction | null = null
-
-	#frame = 0
-
 	/**
 	 * Creates a clip action based on `clipName` for the current model.
 	 * @returns `null` if clip is not found, or model/clip data is not set.
 	 */
-	#getClipAction() {
-		this.clips
-		if (!this.clipName || !this.#mixer) return null
+	#getClipActionEffect(
+		clipName: string | null,
+		clips: THREE.AnimationClip[],
+		mixer: THREE.AnimationMixer,
+	): THREE.AnimationAction | null {
+		if (!clipName) return null
 
-		const clip = THREE.AnimationClip.findByName(this.clips, this.clipName)
+		const clip = THREE.AnimationClip.findByName(clips, clipName)
 		if (!clip) return null
 
-		return this.additive
-			? this.#mixer.clipAction(THREE.AnimationUtils.makeClipAdditive(clip))
-			: this.#mixer.clipAction(clip)
+		return this.additive ? mixer.clipAction(THREE.AnimationUtils.makeClipAdditive(clip)) : mixer.clipAction(clip)
 	}
 
 	connectedCallback() {
 		super.connectedCallback()
 
-		this.isParentModelLoaded = false
-
-		this.clips = []
-
-		this.#mixer = null
-
-		this.#clipAction = null
-
-		// Monitor parent's load state.
 		this.createEffect(() => {
 			// TODO Maybe make this class `Element3D` and use `parentLumeElement` so that it's
 			// reactive.
-			const parent = this.parentElement as any
-			const model = parent?.behaviors?.get?.('gltf-model')?.model
+			const parent = this.parentElement
+			if (!(parent instanceof GltfModel)) throw new Error('<lume-animation> must be a child of a GltfModel')
 
-			if (model) {
-				this.isParentModelLoaded = true
-			} else {
-				this.isParentModelLoaded = false
-				parent?.on?.('MODEL_LOAD', () => {
-					this.isParentModelLoaded = true
+			const parentModelLoaded = onModelLoad(parent)
+
+			createEffect(() => {
+				if (!parentModelLoaded()) return
+
+				const [clips, setClips] = createSignal<THREE.AnimationClip[]>([])
+
+				// Load animations frome external source.
+				createEffect(() => {
+					if (!this.src) {
+						// If no external source, use parent animations (when they are loaded).
+						setClips(this.parentElement!.behaviors?.get?.('gltf-model')?.model?.animations ?? [])
+						return
+					}
+
+					let cleaned = false
+
+					gltfLoader.load(this.src, loadedModel => {
+						if (cleaned) return
+						setClips(loadedModel.animations)
+						// console.log('Loaded external animations:', clips().map(c => c.name))
+					})
+
+					onCleanup(() => (cleaned = true))
 				})
-			}
-		})
 
-		// Load animations frome external source.
-		this.createEffect(() => {
-			if (!this.src) return
+				// When animations load, create mixer, clock, and animation action.
+				createEffect(() => {
+					if (!clips().length) return
 
-			gltfLoader.load(this.src, loadedModel => {
-				this.clips = loadedModel.animations
+					const mixer = new THREE.AnimationMixer((this.parentElement as Element3D).three)
+
+					// Update clip if `additive` changes.
+					createEffect(() => {
+						if (!this.clipName) return
+
+						// TODO: Make this a map of clip actions so they can be cached.
+						const clipAction = this.#getClipActionEffect(this.clipName, clips(), mixer)
+						const clock = new THREE.Clock()
+
+						let frame = 0
+
+						const anim = () => {
+							const delta = clock.getDelta()
+
+							mixer.update(delta)
+
+							const parent = this.parentElement as Element3D
+
+							parent.needsUpdate()
+							parent.scene!.needsUpdate()
+
+							frame = requestAnimationFrame(anim)
+						}
+
+						// Start/stop animation loop when `paused` or `stopped` changes.
+						createEffect(() => {
+							if (this.stopped) {
+								mixer?.stopAllAction()
+								return
+							}
+
+							if (this.paused) return
+
+							// not stopped, not paused, anim is running
+
+							clipAction?.startAt(mixer.time).play()
+
+							if (!frame) frame = requestAnimationFrame(anim)
+							onCleanup(() => (cancelAnimationFrame(frame), (frame = 0)))
+						})
+
+						onCleanup(() => mixer.stopAllAction())
+					})
+				})
 			})
 		})
-
-		// If no external source, use parent animations (when they are loaded).
-		this.createEffect(() => {
-			if (this.isParentModelLoaded && !this.src) {
-				this.clips = this.parentElement!.behaviors?.get?.('gltf-model')?.model?.animations ?? []
-			}
-		})
-
-		this.#frame = 0
-		const anim = () => {
-			this.#frame = requestAnimationFrame(anim)
-
-			// This will keep the animation loop active as long as `paused` is false.
-			if (!this.#mixer || !this.#clipAction || this.clips.length == 0) return
-
-			const delta = this.#clock.getDelta()
-
-			this.#mixer.update(delta)
-
-			const parent = this.parentElement as Element3D
-
-			parent.needsUpdate()
-			parent.scene!.needsUpdate()
-		}
-
-		// When animations load, create mixer, clock, and animation action.
-		this.createEffect(() => {
-			this.clips
-			this.clips.length
-
-			if (!this.isParentModelLoaded) return
-
-			if (this.#mixer) this.#mixer.stopAllAction()
-
-			this.#mixer = new THREE.AnimationMixer((this.parentElement as Element3D).three)
-
-			this.#clock = new THREE.Clock()
-
-			if (!this.clipName) {
-				this.#clipAction = null
-
-				return
-			}
-
-			// Untrack `additive` since it exclusively will update the clip action in another
-			// effect.
-			const clipAction = untrack(() => this.#getClipAction())
-			this.#clipAction = clipAction
-
-			if (untrack(() => !this.paused && !this.stopped)) clipAction?.startAt(this.#mixer.time).play()
-		})
-
-		// Update clip if `additive` changes.
-		this.createEffect(() => {
-			this.#clipAction = this.#getClipAction()
-		})
-
-		// Start/stop animation loop when `paused` or `stopped` changes.
-		this.createEffect(() => {
-			if (this.stopped) {
-				this.#mixer?.stopAllAction()
-
-				return
-			}
-
-			if (!this.paused) {
-				if (!this.#frame) {
-					untrack(() => {
-						this.#frame = requestAnimationFrame(anim)
-					})
-				}
-
-				if (this.#mixer) this.#clipAction?.startAt(this.#mixer.time).play()
-			} else if (this.#frame) {
-				cancelAnimationFrame(this.#frame)
-
-				this.#frame = 0
-			}
-		})
-	}
-
-	disconnectedCallback() {
-		super.disconnectedCallback()
-
-		cancelAnimationFrame(this.#frame)
-
-		this.#frame = 0
 	}
 
 	css = css/*css*/ `
