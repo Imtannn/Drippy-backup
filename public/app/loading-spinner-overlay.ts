3805a602e60325c8669a00766d911cbdbae5c7b1
thidieuanhle <anhlee2101@gmail.com>
2025-10-17 09:14:58 +0200

    feat: update loading animation

---
+++ public/app/loading-spinner-overlay.ts
@@ -0,0 +1,62 @@
+import {css, Element, element, html, type ElementAttributes} from 'lume'
+
+@element
+export class LoadingSpinnerOverlay extends Element {
+	static readonly elementName = 'loading-spinner-overlay'
+
+	template = () => html`
+		<div class="loading-overlay">
+			<div class="spinner"></div>
+		</div>
+	`
+
+	css = css/*css*/ `
+		:host {
+			display: block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: 10;
+			border-radius: 12px;
+			overflow: hidden;
+		}
+
+		.loading-overlay {
+			width: 100%;
+			height: 100%;
+			background: rgba(0, 0, 0, 0.4);
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		.spinner {
+			width: 25px;
+			height: 25px;
+			border: 3px solid rgba(255, 255, 255, 0.3);
+			border-top-color: var(--uiColorAccentViolet);
+			border-radius: 50%;
+			animation: spin 0.8s linear infinite;
+		}
+
+		@keyframes spin {
+			to {
+				transform: rotate(360deg);
+			}
+		}
+	`
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'loading-spinner-overlay': LoadingSpinnerOverlay
+	}
+}
+
+declare module 'lume' {
+	interface IntrinsicElements {
+		'loading-spinner-overlay': ElementAttributes<LoadingSpinnerOverlay>
+	}
+}
