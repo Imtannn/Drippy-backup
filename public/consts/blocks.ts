5133300c84e4aeda6e342933dc688dd442f4bc8c
Thinh Nguyen <dinhthinh.ng123@gmail.com>
2025-12-12 00:43:41 +0700

    Update default garments

--- public/consts/blocks.ts
+++ public/consts/blocks.ts
@@ -6111,7 +6111,7 @@ export const blocks: Record<string, Block[]> = {
 			_id: 'default-shorts',
 			thumb: '',
 			modelFile:
-				'https://drippy3d-prod-eu.s3.eu-west-3.amazonaws.com/drippy-app/drippy-app-3D/models/female/Avatar-1/Default+Garments/Female+-+Default+Shorts.gltf',
+				'https://d1e6s1h8cqcr26.cloudfront.net/drippy-app/drippy-app-3D/models/female/Avatar-1/Default+Garments/Default+Shorts.glb',
 			blockName: 'Default Shorts',
 			avatar: 'female',
 			category: 'Pants',
@@ -6124,7 +6124,7 @@ export const blocks: Record<string, Block[]> = {
 			_id: 'default-top',
 			thumb: '',
 			modelFile:
-				'https://drippy3d-prod-eu.s3.eu-west-3.amazonaws.com/drippy-app/drippy-app-3D/models/female/Avatar-1/Default+Garments/Female+-+Default+Top.gltf',
+				'https://d1e6s1h8cqcr26.cloudfront.net/drippy-app/drippy-app-3D/models/female/Avatar-1/Default+Garments/Default+Top.glb',
 			blockName: 'Default Top',
 			avatar: 'female',
 			category: 'Bodice',
