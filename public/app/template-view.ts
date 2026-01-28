43b10367a3f139dd324017604cda12d12413e62c
Joe Pea <joe@lume.io>
2026-01-27 18:13:21 -0800

    remove garmentsCount properties from collections and spaces

--- public/app/template-view.ts
+++ public/app/template-view.ts
@@ -4,7 +4,7 @@ import {getTemplatesByCollection} from '../consts/templates.js'
 import {onboardingStyles} from '../styles/onboarding-styles.js'
 import type {Template, TemplateCategory} from '../types/template.js'
 import type {Collection, TemplateMap} from '../types/types.js'
-import {getCollectionBySlug, getSpaceCollections, spaceHasMultipleCollections, values} from '../utils.js'
+import {getCollectionBySlug, getSpaceCollectionSlugs, spaceHasMultipleCollections, values} from '../utils.js'
 import {
 	currentUser,
 	isLoggedIn,
@@ -105,7 +105,7 @@ export class TemplateView extends Element {
 			if (this.spaceCollection) collectionTemplates = getTemplatesByCollection(this.spaceCollection)
 			else {
 				// If no collection selected and multi-collection space, aggregate from all collections
-				const spaceCollections = getSpaceCollections(store.selectedSpace)
+				const spaceCollections = getSpaceCollectionSlugs(store.selectedSpace)
 				for (const collectionSlug of spaceCollections)
 					collectionTemplates.push(...getTemplatesByCollection(collectionSlug))
 			}
@@ -756,7 +756,9 @@ export class TemplateView extends Element {
 												>
 													<for-each
 														items=${() =>
-															getSpaceCollections(store.selectedSpace).map(c => getCollectionBySlug(collections(), c))}
+															getSpaceCollectionSlugs(store.selectedSpace).map(c =>
+																getCollectionBySlug(collections(), c),
+															)}
 														content=${() => (collection: Collection) => html`
 															<button
 																draggable=${false}
