4549ecddcb97229518fceaa91d25be5203eff924
Thinh Nguyen <dinhthinh.ng123@gmail.com>
2026-01-28 11:52:38 +0700

    Update OOFYA scene and env

--- public/consts/scenes.ts
+++ public/consts/scenes.ts
@@ -8,8 +8,8 @@ export const legacyBackgroundScenes: BackgroundScene[] = [
 		name: 'Metamorphosis',
 		slug: 'metamorphosis',
 		description: 'OOFYA',
-		env: '/images/envs/industrial_wooden_attic.jpg',
-		scene: 'https://d1e6s1h8cqcr26.cloudfront.net/drippy-app/drippy-app-3D/brands/oofya/OOFYA+SCENE-light1.glb',
+		env: '/images/envs/OOFYA-env.webp',
+		scene: 'https://d1e6s1h8cqcr26.cloudfront.net/drippy-app/drippy-app-3D/brands/oofya/OOFYA+-+Scene.glb',
 		// scene: '/models/OOFYA+SCENE-light+(4).glb',
 		includedModelFiles: [
 			// 'https://d1e6s1h8cqcr26.cloudfront.net/drippy-app/drippy-app-3D/brands/oofya/OOFYA+-+SHOES.glb',
